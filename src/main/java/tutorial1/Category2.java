a8a5ec2b72794401cfe4eb4156751575421898e6
Maikel Langezaal <maikellangezaal@hotmail.com>
2017-10-24 12:30:40 +0200

    added metric removed or added file

--- src/main/java/tutorial1/Category2.java
+++ src/main/java/tutorial1/Category2.java
@@ -14,6 +14,8 @@ import org.repodriller.scm.SCMRepository;
 public class Category2 implements CommitVisitor { 
 	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
 		for(Modification m : commit.getModifications()) {
+			if(!m.getType().name().equals("MODIFY"))
+				System.out.println(m.getType().name());
 			writer.write(
 					commit.getHash(),
 					m.getFileName(),
@@ -21,7 +23,9 @@ public class Category2 implements CommitVisitor {
 					m.getRemoved(),
 					//getLA(commit, m),
 					//getLD(commit, m),
-					getLT(commit, m)
+					getLT(commit, m),
+					isAdded(m),
+					isRemoved(m)
 			);
 		}
 	}
@@ -84,7 +88,7 @@ public class Category2 implements CommitVisitor {
 	 */
 	public int getLT(Commit commit,  Modification m) {
 		String sourceCode = m.getSourceCode();
-		int count = 1;
+		int count = 0;
 		while(sourceCode.contains("\n")) {
 			sourceCode = sourceCode.substring(sourceCode.indexOf("\n") + 1);
 			count++;
@@ -92,6 +96,17 @@ public class Category2 implements CommitVisitor {
 		return count;
 	}
 	
+	public int isAdded(Modification m) {
+		if(m.getType().name().equals("ADD"))
+			return 1;
+		return 0;
+	}
+	
+	public int isRemoved(Modification m) {
+		if(m.getType().name().equals("DELETE"))
+			return 1;
+		return 0;
+	}
 }
 	
 
--- src/main/java/tutorial1/Q1Study.java
+++ src/main/java/tutorial1/Q1Study.java
@@ -1,6 +1,8 @@
 package tutorial1;
 
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.BasicConfigurator;
 import org.repodriller.RepoDriller;
@@ -13,21 +15,18 @@ import org.repodriller.scm.GitRepository;
 
 public class Q1Study implements Study {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		BasicConfigurator.configure();
 		new RepoDriller().start(new Q1Study());
 	}
 	
 	
-	public void execute() {
-		
-		
+	public void execute() {		
 		Calendar date = Calendar.getInstance();
 		date.set(2016, 0, 1);
 		
-		startCat1(date);
+		//startCat1(date);
 		startCat2(date);
-		
 	}
 	
 	public void startCat1(Calendar date) {
@@ -54,6 +53,18 @@ public class Q1Study implements Study {
 		.mine();
 	}
 	
+	public void startBugMiner(Calendar date) {
+		PersistenceMechanism writer3 = new CSVFile("csv/bugzillaBugIDS.csv");
+		initWriter3(writer3);
+		
+		new RepositoryMining()
+		.in(GitRepository.singleProject("projects/bugzilla"))
+		.through(Commits.since(date))
+		.withThreads(4)
+		.process(new BugzillaMiner(), writer3)
+		.mine();
+	}
+	
 	public void initWriter(PersistenceMechanism writer) {
 		writer.write(
 				"Hash",
@@ -71,7 +82,16 @@ public class Q1Study implements Study {
 				"File",
 				"LA",
 				"LD",
-				"LT"
+				"LT",
+				"isAdded",
+				"isRemoved"
+		);
+	}
+	
+	public void initWriter3(PersistenceMechanism writer) {
+		writer.write(
+				"Hash",
+				"ID"
 		);
 	}
 
